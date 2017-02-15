e1d7c90919653b3877645101b5cf34d3d11c1ab4
OscarGB <oscar.gomez.borz@gmail.com>
2017-02-15 19:14:36 +0100

    clase de hoy

---
+++ Clases/src/clase20170215/Cancion.java
@@ -0,0 +1,12 @@
+package clase20170215;
+
+public class Cancion extends Abstracta {
+	private double duration;
+	
+	public Cancion(String title, double duration, int year) {
+		this.title = title;
+		this.duration = duration;
+		this.year = year;
+	}
+	
+}
