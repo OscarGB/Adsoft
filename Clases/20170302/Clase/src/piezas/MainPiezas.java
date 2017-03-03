29a521d06ab9e25e153301dd1da2a104c3c86765
OscarGB <oscar.gomez.borz@gmail.com>
2017-03-03 17:18:17 +0100

    cosas

---
+++ Clases/20170302/Clase/src/piezas/MainPiezas.java
@@ -0,0 +1,21 @@
+/* Crea un programa java que poermita crear Piezas con un determinado peso
+ * A cada pieza se le asignará automáticamente un identificador único.
+ * La clase pieza debe contener un método estático que devuelva la pieza
+ * más pesada creada hasta el momento.
+*/
+
+
+package piezas;
+
+public class MainPiezas {
+
+	public static void main(String[] args){
+		Pieza p1 = new Pieza(4.5);
+		Pieza p2 = new Pieza(5);
+		Pieza p3 = new Pieza(0.5);
+		
+		System.out.println(p1 + "\n" + p2 + "\n" + p3);
+		System.out.println("Mas pesada: " + Pieza.masPesada());
+	}
+	
+}
